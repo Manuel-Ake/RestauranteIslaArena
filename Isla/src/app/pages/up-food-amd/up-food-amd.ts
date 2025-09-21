5d494d813b486a364ad29c0abb6173d543f6ab8e
JosiasET <josiasneftaliektun@gmail.com>
2025-09-21 13:04:37 -0600

    UpFood

--- Isla/src/app/pages/up-food-amd/up-food-amd.ts
+++ Isla/src/app/pages/up-food-amd/up-food-amd.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { foodService } from '../../core/service/foodService';
+import { Saucer } from '../../core/models/saucer';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-up-food-amd',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './up-food-amd.html',
   styleUrl: './up-food-amd.css'
 })
 export class UpFoodAmd {
+  activeSection: String = 'upfood';
 
+  setSection(section: string) {
+    this.activeSection = section;
+
+  }
+  nombre = '';
+  descripcion = '';
+  precio!: number;
+  imageBase64: string = '';
+
+  constructor(private foodservice: foodService){}
+
+  OnfileSelected(event: any){
+    const file = event.target.files[0];
+    if(file){
+      const reader = new FileReader();
+      reader.onload = () =>{
+        this.imageBase64 = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  subirsaucer(){
+    const newsaucer: Saucer = {
+      nombre: this.nombre,
+      descripcion: this.descripcion,
+      precio: this.precio,
+      imagen: this.imageBase64
+    };
+
+    alert("Platillo subido exitosamente" + newsaucer);
+    this.foodservice.agregarPlatillo(newsaucer);
+
+  }
 }
--- Isla/src/app/shared/gestoramd/gestoramd.ts
+++ Isla/src/app/shared/gestoramd/gestoramd.ts
@@ -2,54 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 /*import { Router } from '@angular/router';*/
-import { Saucer } from '../../core/models/saucer';
-import { foodService } from '../../core/service/foodService';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-gestoramd',
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
   templateUrl: './gestoramd.html',
   styleUrl: './gestoramd.css'
 })
 export class Gestoramd {
-  // para navegar en las secciones
-  activeSection: string = 'resumen';
-
-  setSection(section: string) {
-    this.activeSection = section;
-  }
-
-  nombre = '';
-  descripcion = '';
-  precio!: number;
-  imageBase64: string = '';
-
-  constructor(private foodservice:foodService){}
-
-  OnfileSelected(event: any){
-    const file = event.target.file[0];
-    if(file){
-      const reader = new FileReader();
-      reader.onload = () =>{
-        this.imageBase64 = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-  subirsaucer(){
-    const newsaucer: Saucer = {
-      nombre: this.nombre,
-      descripcion: this.descripcion,
-      precio: this.precio,
-      imagen: this.imageBase64
-    };
-
-    alert("Platillo subido exitosamente" + newsaucer);
-    this.foodservice.agregarPlatillo(newsaucer);
-
-  }
+  
 }
