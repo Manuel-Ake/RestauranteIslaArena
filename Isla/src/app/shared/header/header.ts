46da9744db806b9b1a8fce366e7c1a5cf58be8ca
Rubissell <8456@itescam.edu.mx>
2025-09-21 12:24:49 -0600

    descanso

--- Isla/src/app/shared/header/header.ts
+++ Isla/src/app/shared/header/header.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "../../pages/home/home";
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink,CommonModule, RouterOutlet, Home],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+   homeActive: boolean = true; // INICIO empieza encendido
 
+  constructor(private router: Router) {}
+
+  goHome() {
+    if (!this.homeActive) {
+      this.homeActive = true;
+      this.router.navigateByUrl(''); // Navega a la raíz (home)
+    }
+    // Si ya está activo, no hace nada (evita sobrecarga)
+  }
+
+  setHome(state: boolean) {
+    this.homeActive = state;
+  }
 }
